7001c2dc9194803dff276d308a4cdec2f194ee55
Katrina Shainiak <ekaterina.shainiak@gmail.com>
2018-01-06 16:12:32 -0800

    changed pictures and <spam> to <img> tag for every Card

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,9 +1,15 @@
 import React from "react";
 import "./Card.css";
 
+// const Card = props => (
+//     <div className="img-container">
+//         <div alt={props.name} style={{backgroundImage: `url(${props.image})`}} />
+//     </div>
+// );
+
 const Card = props => (
     <div className="img-container">
-        <div alt={props.name} style={{backgroundImage: `url(${props.image})`}} />
+        <img alt={props.name} src={props.image} />
     </div>
 );
 
