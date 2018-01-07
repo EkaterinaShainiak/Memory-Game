cca7ea9a5d7268c8e4b72a645c11a02b39215367
Katrina Shainiak <ekaterina.shainiak@gmail.com>
2018-01-06 23:18:36 -0800

    moved pics to local folder, added README.md

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,12 +1,6 @@
 import React from "react";
 import "./Card.css";
 
-// const Card = props => (
-//     <div className="img-container">
-//         <div alt={props.name} style={{backgroundImage: `url(${props.image})`}} />
-//     </div>
-// );
-
 const Card = props => (
     <div className={`img-container ${props.shaken ? "shaken" : ""}`}>
         <img alt={props.name} src={props.image} onClick={props.onclick}/>
